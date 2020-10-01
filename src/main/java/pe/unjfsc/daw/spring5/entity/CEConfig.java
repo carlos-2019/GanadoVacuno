5658f7ad99023b6456e9b89f53c00bbf4b62f078
francisco-pho <57578912+francisco-pho@users.noreply.github.com>
2020-10-01 10:53:22 -0500

    Update CEConfig.java

--- src/main/java/pe/unjfsc/daw/spring5/entity/CEConfig.java
+++ src/main/java/pe/unjfsc/daw/spring5/entity/CEConfig.java
@@ -16,4 +16,14 @@ public class CEConfig {
 	@Bean public CMGanadoVacuno idCMGanadoVacuno() {
 		return new CMGanadoVacuno();
 	}
+	
+	//beanes de recria
+	@Bean
+	public CERecria idRecria() {
+		return new CERecria();
+	}
+	@Bean
+	public CMRecria idCMRecria() {
+		return new CMRecria();
+	}
 }
